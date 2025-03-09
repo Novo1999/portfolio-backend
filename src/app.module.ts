e274bd53488a650b03432132f0d695f217b52386
Novo1999 <neptune.navo@gmail.com>
2025-03-09 14:23:21 +0600

    auth added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppService } from 'src/app.service';
 import { DatabaseModule } from 'src/DbModule/db';
 import { AppController } from './app.controller';
+import { AuthModule } from './auth/auth.module';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AuthModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
