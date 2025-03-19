904bff882d8ab82d98762936e05dd2ed5c20722e
Novo1999 <neptune.navo@gmail.com>
2025-03-19 10:39:20 +0600

    fix: project edit

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { Project } from 'src/projects/project.dto';
 import { ProjectsService } from 'src/projects/projects.service';
 
 @Controller('projects')
@@ -8,5 +18,23 @@ export class ProjectsController {
   @Get(':username')
   async getProjects(@Param() { username }: Record<string, string>) {
     return this.projectsService.getProjects(username);
+  }
+
+  @UseGuards(AuthGuard)
+  @Post(':username')
+  async addProject(
+    @Param() { username }: Record<string, string>,
+    @Body() project: Project,
+  ) {
+    return this.projectsService.addProject(username, project);
+  }
+
+  @UseGuards(AuthGuard)
+  @Put(':id')
+  async editProject(
+    @Param() { id }: Record<string, string>,
+    @Body() project: Project,
+  ) {
+    return this.projectsService.editProject(id, project);
   }
 }
--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,4 +1,10 @@
-import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { Project } from 'src/projects/project.dto';
 
 @Injectable()
@@ -30,6 +36,32 @@ export class ProjectsService {
     } catch (error) {
       if (error)
         throw new HttpException('Error adding project', HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  async editProject(id: string, project: Project) {
+    try {
+      const expRow = await this.sql`
+      SELECT EXISTS(SELECT 1 FROM projects WHERE id=${id})`;
+
+      const expExists = expRow[0]?.exists;
+
+      if (!expExists) throw new NotFoundException('Project Not Found');
+
+      const editedProject = await this.sql`
+      UPDATE projects
+      SET
+       title = ${project.title},
+       tech_stack = ${project.tech_stack},
+       image_url = ${project.image_url},
+       github_client_url = ${project.github_client_url},
+       github_server_url = ${project.github_server_url},
+       live_url = ${project.live_url}
+      WHERE id = ${id}
+      RETURNING *`;
+      return editedProject;
+    } catch (error) {
+      if (error) throw new HttpException(error, HttpStatus.BAD_REQUEST);
     }
   }
 }
